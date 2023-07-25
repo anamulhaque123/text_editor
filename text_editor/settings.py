d26e42c2e1cdf718a680bc2b6586acf40fce7ea6
anamul <anamul@atilimited.net>
2023-07-25 15:20:06 +0600

    commit text

--- text_editor/settings.py
+++ text_editor/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'notification.context_processors.notifications',
+                # 'notification.context_processors.notifications',
             ],
         },
     },
@@ -168,7 +168,7 @@ MESSAGE_LEVEL = 10
 
 DJANGO_NOTIFICATIONS_CONFIG = { 'SOFT_DELETE': True}
 
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 
 
